02c115f486c82777279c523655a0586834cd94d6
Paul Soteropulos <complexsimplicity22@yahoo.com>
2022-12-13 21:57:58 -0600

    updated stupid react landing

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>Welcome to CookBook</h1>
+        <h4>I just cooked a book. It was very bland.</h4>
+      </div>
     </div>
   );
 }
