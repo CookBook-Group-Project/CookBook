7a9756d6d64cb5450d71dfed91b8d226a016d218
Christopher Shaw <silvertn350@yahoo.com>
2022-12-20 09:21:26 -0600

    styled recipe card

--- client/src/components/Recipe-Card/RecipeCard.js
+++ client/src/components/Recipe-Card/RecipeCard.js
@@ -1,41 +1,73 @@
-import React from 'react'
+import Nav from '../Nav/Nav'
+import './RecipeCard.css'
 
 const RecipeCard = (props) => {
 
-    //@CHRIS - Go ahead and delete all the styling down there. I've just been using it to help visualize what's being built. - GL 17DEC2022
+
     //We pull down the list of all recipes via props so that cards can be generated for each.
     const {recipe} = props
 
-    //Placeholder style for the card container
-    const cardStyle = {
-        "background": "grey",
-        "height": "300px",
-        "width": "200px",
-        "display": "flex",
-        "flex-direction": "column",
-        "text-align": "center"
+
+    // Switching between image and directions view 
+    const handleClick = () =>{
+        const recImg = document.querySelector('.image-container')
+        const directions = document.querySelector('.directions-container')
+        const show = document.querySelector('.show-directions')
+
+        recImg.style.display = 'none'
+        directions.style.display = 'block'
+        show.innerHTML = 'Currently Cooking'
     }
-    
-    //Placeholder style for time image
-    const imageStyle = {
-        "object-fit": "cover",
-        "height": "280px",
-        "width": "180px",
-        "padding": "20px"
-    }
+
+
 
     return (
         <div>
-            {/*Grant 17DEC2022 - The block below is used to help see work being done on the component due to the navigation styling. This can be deleted once complete. */}
-            <div style={{height:"90px", background: "white"}}></div>
-            <h1>TEST</h1>
-            {/* Recipe cards that will be mapped out on the screen */}
-            <div style={cardStyle}>
-                {/* <p>{recipe.title}</p> 12/19 CS commented out, threw error can not read 'title' */}
-                {/* <p>{recipe.creator}</p> 12/18 PS testing */}
-                {/* <p>{recipe.creator.username}</p> 12/18 PS testing */}
-                <img style={imageStyle} src='https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg' alt="Recipe image"/>
+            <Nav></Nav>
+            {/* Overall Container  */}
+            <div className='recipe-card-container'>
+            {/* Left Container */}
+                <div className='recipe-card-left'>
+                    <div className='left-title'>
+                        <h1>Directions and</h1>
+                        <h2>ingredients.</h2>
+                    </div>
+                    <div className='recipe-card'>
+                        <div className='recipe-card-top'>
+                            <h3>Servings 
+                                <span className='card-span'>4</span>
+                            </h3>
+                            <h3>Prep Time 
+                                <span className='card-span'>20 mins</span>
+                            </h3>
+                            <h3>Cook Time 
+                                <span className='card-span'>40 mins</span>
+                            </h3>
+                        </div>
+                        <div className='recipe-card-bottom'>
+                            <h1>Ingredients Needed</h1>
+                            <p>Ingredients list goes here</p>
+                        </div>
+                        <h2 className='show-directions' onClick={handleClick}>Show Directions</h2>
+                    </div>
+                </div>
+                {/* Right Container  */}
+                <div className='recipe-card-right'>
+                <div className='card-right-container'>
+                    <h3 className='recipe-title'>Recipe Title</h3>
+                    <hr className='recipe-title-divider'></hr>
+                    <div className='image-container'>
+                        <img src='https://images.unsplash.com/photo-1540189549336-e6e99c3679fe?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8Zm9vZHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60' alt='recipe image' className='recipe-card-image'></img>
+                    </div>
+                    <div className='directions-container'>
+                    <p>directions</p>
+                    <p>directions</p>
+                    <p>directions</p>
+                    </div>
+                </div>
+                </div>
             </div>
+
         </div>
     )
 }
